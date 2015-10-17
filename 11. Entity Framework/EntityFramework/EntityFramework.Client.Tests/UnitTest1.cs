e45e29d6762629437ec89169411db8c5f5a7b053
dchakov <dchakov@yahoo.com>
2015-10-17 11:45:01 +0300

    EntityFramework initial

---
+++ 11. Entity Framework/EntityFramework/EntityFramework.Client.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EntityFramework.Client.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ 11. Entity Framework/EntityFramework/EntityFramework.Client/OrdersToCanada.cs
@@ -0,0 +1,8 @@
+﻿namespace EntityFramework.Client
+{
+    public class OrdersToCanada
+    {
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+    }
+}
